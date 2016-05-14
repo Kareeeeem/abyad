3ac374ab255e9d426ddc8a1c158a32ee41f56957
Mohammed Kareem <kareeeeem@gmail.com>
2016-05-15 01:37:25 +0200

    testing with real files

---
+++ tests/programs.py
@@ -0,0 +1,103 @@
+import os
+from collections import namedtuple
+
+from src.tokens import SPACE, TAB, LF
+from src import utils
+
+Program = namedtuple('Program', 'name program stack')
+programs = []
+
+stack_manipulation = Program(
+    'stack_manipulation',
+    utils.join(
+        SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,  # push 5
+        SPACE, SPACE, SPACE, TAB, TAB, TAB, LF,  # push 7
+        SPACE, LF, TAB,  # swap
+        SPACE, LF, SPACE,  # duplicate
+        SPACE, SPACE, SPACE, TAB, SPACE, SPACE, TAB, LF,  # push 9
+        SPACE, LF, TAB,  # swap
+        SPACE, LF, LF,  # pop
+        LF, LF, LF,  # terminate
+    ),
+    stack=[7, 5, 9]
+)
+
+programs.append(stack_manipulation)
+
+arithmetic = Program(
+    'arithmetic',
+    utils.join(
+        SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,  # push 5
+        SPACE, SPACE, SPACE, TAB, TAB, TAB, LF,  # push 7
+        TAB, SPACE, SPACE, SPACE,  # add - result = 12
+        SPACE, SPACE, SPACE, TAB, SPACE, SPACE, TAB, LF,  # push 9
+        TAB, SPACE, SPACE, TAB,  # sub - result = 3
+        SPACE, SPACE, SPACE, TAB, SPACE, LF,  # push 2
+        TAB, SPACE, TAB, TAB,  # mod - result = 1
+        SPACE, SPACE, SPACE, TAB, SPACE, SPACE, TAB, LF,  # push 9
+        SPACE, LF, TAB,  # swap
+        TAB, SPACE, TAB, SPACE,  # div - result = 9
+        LF, LF, LF,  # terminate
+    ),
+    stack=[9]
+)
+programs.append(arithmetic)
+
+count = Program(
+    'count',
+    # this is the example program from compsoc.dur.ac.uk/whitespace/tutorial.html
+    # without the IO.
+    utils.join(
+        SPACE, SPACE, SPACE, TAB, LF,
+        LF, SPACE, SPACE, SPACE, TAB, SPACE, SPACE, SPACE, SPACE, TAB, TAB, LF,
+        SPACE, SPACE, SPACE, TAB, LF,
+        TAB, SPACE, SPACE, SPACE,
+        SPACE, LF, SPACE,
+        SPACE, SPACE, SPACE, TAB, SPACE, TAB, TAB, LF,
+        TAB, SPACE, SPACE, TAB,
+        LF, TAB, SPACE, SPACE, TAB, SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,
+        LF, SPACE, LF, SPACE, TAB, SPACE, SPACE, SPACE, SPACE, TAB, TAB, LF,
+        LF, SPACE, SPACE, SPACE, TAB, SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,
+        SPACE, LF, LF,
+        LF, LF, LF,
+    ),
+    stack=[]
+)
+
+
+programs.append(count)
+
+terminate = Program(
+    'terminate',
+    utils.join(
+        SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,
+        SPACE, SPACE, SPACE, TAB, TAB, TAB, LF,
+        SPACE, LF, TAB,
+        LF, LF, LF,  # terminate
+        SPACE, SPACE, SPACE, TAB, TAB, TAB, LF,  # attempt to push again
+    ),
+    stack=[7, 5]
+)
+programs.append(terminate)
+
+heap = Program(
+    'heap',
+    utils.join(
+        SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,  # push 5
+        SPACE, SPACE, SPACE, TAB, TAB, TAB, LF,  # push 7
+        TAB, TAB, SPACE,  # store 7 at 5
+        SPACE, SPACE, SPACE, TAB, SPACE, SPACE, TAB, LF,  # push 9
+        SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,  # push 5
+        TAB, TAB, TAB,  # retrieve 7
+        TAB, SPACE, SPACE, TAB,  # sub  result = 2
+        LF, LF, LF,  # terminate
+    ),
+    stack=[2]
+)
+programs.append(heap)
+
+
+def tows(program, name):
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(basedir, name + '.ws'), 'wb') as f:
+        f.write(program)
--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,92 +1,50 @@
-from src import utils
+import os
 from src import interpreter
 from src import lib
-from src.tokens import SPACE, TAB, LF
+import programs
+
+
+for program in programs.programs:
+    programs.tows(program.program, program.name)
+
+
+def read(name):
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(basedir, name + '.wso'), 'rb') as f:
+        program = f.read()
+    return program
 
 
 def test_stack_manipulation_program():
     state = lib.State()
-    program = utils.join(
-        SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,  # push 5
-        SPACE, SPACE, SPACE, TAB, TAB, TAB, LF,  # push 7
-        SPACE, LF, TAB,  # swap
-        SPACE, LF, SPACE,  # duplicate
-        SPACE, SPACE, SPACE, TAB, SPACE, SPACE, TAB, LF,  # push 9
-        SPACE, LF, TAB,  # swap
-        SPACE, LF, LF,  # pop
-        LF, LF, LF,  # terminate
-    )
-
+    program = read(programs.stack_manipulation.name)
     interpreter.eval(program, state)
-    assert state.stack == [7, 5, 9]
+    assert state.stack == programs.stack_manipulation.stack
 
 
 def test_stack_arithmetic_program():
     state = lib.State()
-    program = utils.join(
-        SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,  # push 5
-        SPACE, SPACE, SPACE, TAB, TAB, TAB, LF,  # push 7
-        TAB, SPACE, SPACE, SPACE,  # add - result = 12
-        SPACE, SPACE, SPACE, TAB, SPACE, SPACE, TAB, LF,  # push 9
-        TAB, SPACE, SPACE, TAB,  # sub - result = 3
-        SPACE, SPACE, SPACE, TAB, SPACE, LF,  # push 2
-        TAB, SPACE, TAB, TAB,  # mod - result = 1
-        SPACE, SPACE, SPACE, TAB, SPACE, SPACE, TAB, LF,  # push 9
-        SPACE, LF, TAB,  # swap
-        TAB, SPACE, TAB, SPACE,  # div - result = 9
-        LF, LF, LF,  # terminate
-    )
+    program = read(programs.arithmetic.name)
     interpreter.eval(program, state)
-    assert state.stack == [9]
+    assert state.stack == programs.arithmetic.stack
 
 
-def test_stack_heap_access_program():
+def test_heap_program():
     state = lib.State()
-    program = utils.join(
-        SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,  # push 5
-        SPACE, SPACE, SPACE, TAB, TAB, TAB, LF,  # push 7
-        TAB, TAB, SPACE,  # store 7 at 5
-        SPACE, SPACE, SPACE, TAB, SPACE, SPACE, TAB, LF,  # push 9
-        SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,  # push 5
-        TAB, TAB, TAB,  # retrieve 7
-        TAB, SPACE, SPACE, TAB,  # sub  result = 2
-        LF, LF, LF,  # terminate
-    )
+    program = read(programs.heap.name)
     interpreter.eval(program, state)
-    assert state.stack == [2]
+    assert state.stack == programs.heap.stack
 
 
-def test_mark_program():
+def test_count_program():
     state = lib.State()
-    # this is the example program from compsoc.dur.ac.uk/whitespace/tutorial.html
-    # without the IO.
-    program = utils.join(
-        SPACE, SPACE, SPACE, TAB, LF,
-        LF, SPACE, SPACE, SPACE, TAB, SPACE, SPACE, SPACE, SPACE, TAB, TAB, LF,
-        SPACE, SPACE, SPACE, TAB, LF,
-        TAB, SPACE, SPACE, SPACE,
-        SPACE, LF, SPACE,
-        SPACE, SPACE, SPACE, TAB, SPACE, TAB, TAB, LF,
-        TAB, SPACE, SPACE, TAB,
-        LF, TAB, SPACE, SPACE, TAB, SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,
-        LF, SPACE, LF, SPACE, TAB, SPACE, SPACE, SPACE, SPACE, TAB, TAB, LF,
-        LF, SPACE, SPACE, SPACE, TAB, SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,
-        SPACE, LF, LF,
-        LF, LF, LF,
-    )
+    program = read(programs.count.name)
     interpreter.eval(program, state)
-    assert state.stack == []
+    assert state.stack == programs.count.stack
 
 
 def test_program_termination():
     state = lib.State()
-    program = utils.join(
-        SPACE, SPACE, SPACE, TAB, SPACE, TAB, LF,
-        SPACE, SPACE, SPACE, TAB, TAB, TAB, LF,
-        SPACE, LF, TAB,
-        LF, LF, LF,  # terminate
-        SPACE, SPACE, SPACE, TAB, TAB, TAB, LF,  # attempt to push again
-    )
-
+    program = read(programs.terminate.name)
     interpreter.eval(program, state)
-    assert state.stack == [7, 5]
+    assert state.stack == programs.terminate.stack
