61795c255c5353b6b6a52b3d0efbc4d6cb43a542
Mohammed Kareem <kareeeeem@gmail.com>
2016-05-17 12:53:31 +0200

    cleaner tokens

--- src/tokens.py
+++ src/tokens.py
@@ -1,38 +1,32 @@
 def join(*ws):
     return ('').join(ws)
 
-SPACE = ' '
-TAB = '\t'
-LF = '\n'
+SPACE, TAB, LF = ' ', '\t', '\n'
+STACK_MANIPULATION = SPACE
+ARITHMETIC = TAB + SPACE
+FLOW_CONTROL = LF
+HEAP_ACCESS = TAB + TAB
+IO = TAB + LF
 
-# Stack manipulation
-PUSH = join(SPACE, SPACE)
-DUP = join(SPACE, LF, SPACE)
-SWAP = join(SPACE, LF, TAB)
-POP = join(SPACE, LF, LF)
-
-# Arithmetic
-ADD = join(TAB, SPACE, SPACE, SPACE)
-SUB = join(TAB, SPACE, SPACE, TAB)
-MUL = join(TAB, SPACE, SPACE, LF)
-DIV = join(TAB, SPACE, TAB, SPACE)
-MOD = join(TAB, SPACE, TAB, TAB)
-
-# Flow_control
-MARK = join(LF, SPACE, SPACE)
-JUMP = join(LF, SPACE, LF)
-JUMP_Z = join(LF, TAB, SPACE)
-JUMP_N = join(LF, TAB, TAB)
-EXIT = join(LF, LF, LF)
-CALL = join(LF, SPACE, TAB)
-END = join(LF, TAB, LF)
-
-# Heap access
-STORE = join(TAB, TAB, SPACE)
-LOAD = join(TAB, TAB, TAB)
-
-# IO
-OUTC = join(TAB, LF, SPACE, SPACE)
-OUTI = join(TAB, LF, SPACE, TAB)
-INC = join(TAB, LF, TAB, SPACE)
-INI = join(TAB, LF, TAB, TAB)
+PUSH = join(STACK_MANIPULATION, SPACE)
+DUP = join(STACK_MANIPULATION, LF, SPACE)
+SWAP = join(STACK_MANIPULATION, LF, TAB)
+POP = join(STACK_MANIPULATION, LF, LF)
+ADD = join(ARITHMETIC, SPACE, SPACE)
+SUB = join(ARITHMETIC, SPACE, TAB)
+MUL = join(ARITHMETIC, SPACE, LF)
+DIV = join(ARITHMETIC, TAB, SPACE)
+MOD = join(ARITHMETIC, TAB, TAB)
+MARK = join(FLOW_CONTROL, SPACE, SPACE)
+JUMP = join(FLOW_CONTROL, SPACE, LF)
+JUMP_Z = join(FLOW_CONTROL, TAB, SPACE)
+JUMP_N = join(FLOW_CONTROL, TAB, TAB)
+EXIT = join(FLOW_CONTROL, LF, LF)
+CALL = join(FLOW_CONTROL, SPACE, TAB)
+END = join(FLOW_CONTROL, TAB, LF)
+STORE = join(HEAP_ACCESS, SPACE)
+LOAD = join(HEAP_ACCESS, TAB)
+OUTC = join(IO, SPACE, SPACE)
+OUTI = join(IO, SPACE, TAB)
+INC = join(IO, TAB, SPACE)
+INI = join(IO, TAB, TAB)
